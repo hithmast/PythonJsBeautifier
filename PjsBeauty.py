038de01193aa7461d005f482cfb54f7aacd52886
AliEmara <70377134+hithmast@users.noreply.github.com>
2020-11-18 13:21:21 +0200

    Create PjsBeauty.py

---
+++ PjsBeauty.py
@@ -0,0 +1,29 @@
+import requests #IMPORT PyLIBRARY
+import jsbeautifier
+import os
+
+url = input("Enter URL : ")
+url_ = str(url)
+param1 = input("Enter Param: ") #ADD more Parameters if you need 
+param1_value = input("Enter Param. VALUE : ") #MORe Values TOO ^_^ 
+r = requests.get(url_,data = {param1:param1_value}) #MAKING HTTP GET REQUEST 
+
+code =r.status_code #RESPONSE CODE :
+headers = r.headers 
+encodeing =r.encoding
+text = r.text
+print("Code: ",code)
+print("headers : ",headers)
+print("encoding : ",encodeing)
+print("Text : ",text)
+
+print("BEAUTIFY ")
+
+res = jsbeautifier.beautify(text)
+if True:
+	print("DONE++")	
+	print(res,file=open("beautified.txt","x")) #cat TEXT File for Results
+
+else:
+	print("FAILED")
+
